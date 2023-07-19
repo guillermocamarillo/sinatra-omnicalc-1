a760edb5141bef56e85b6178fc5c1d668d57627f
guillermocamarillo <137141464+guillermocamarillo@users.noreply.github.com>
2023-07-19 02:27:12 +0000

    payment

--- app.rb
+++ app.rb
@@ -14,7 +14,32 @@ get '/square/results' do
 end
 
 get("/square_root/new") do
+  erb(:new_square_root)
+end
+
+get'/square_root/results' do
+  @input = params.fetch("user_number")
+  @input_root = Math.sqrt(@input.to_f)
+  erb(:square_root_results)
 end
 
 get("/payment/new") do
+  erb(:new_payment)
+end
+
+get '/payment/results' do
+  @apr = params.fetch("user_apr").to_f.round(4)
+  @aprWithPercentSign = @apr.to_s + "%"
+  @years = params.fetch("user_years").to_f
+  @principal = params.fetch("user_pv").to_f.round(2)
+  @principalFormatted = "$" + @principal.to_s
+  aprDecimal = @apr/100
+  aprDecimalMonthly = aprDecimal / 12
+  numerator = aprDecimalMonthly * @principal
+  totalMonths = 12 * @years
+  aprPlusOne = aprDecimalMonthly + 1
+  denominator = 1 - (aprPlusOne**-totalMonths)
+  payment = numerator / denominator
+  @monthlyPayment = "$" + payment.round(2).to_s
+  erb(:payment_results)
 end
